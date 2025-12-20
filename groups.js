764e611cef86e86abfb930182f44e91e44933381
Cassie Bezick <cassiebezick@gmail.com>
2025-12-19 19:39:14 -0500

    Add files via upload

--- groups.js
+++ groups.js
@@ -3,44 +3,43 @@ const API_KEY = "live_c1LVRF88KDghAq5ImtSiz23of0wDNtl17oANVIuJIdwH3NFG5AZaAAv9eZ
 
 const dogBreedContainer = document.querySelector(".dogbreed-list");
 
+let breeds = [];
 
 async function fetchData() {
-  const response = await fetch(API_URL, {
+  let response = await fetch(API_URL, {
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${API_KEY}`
     }
   });
-
-const dogBreeds = await response.json();
- // console.log(dogBreeds); // always inspect the data
-  
-dogBreedContainer.innerHTML = dogBreeds
-    .map(breed => dogHTML(breed))
-    .join("");
-
-function filterByDogBreeds(filter) {
-  return dogBreeds.filter(
-    breed => breed.breed_group === filter);
+  breeds = await response.json();
 };
-}
-
-
-function dogHTML(breed) {
-  return `
-  <div class="user">
-    <div class="user-card">
-    <div class="user-card__container">
-      <h3>${breed.name}</h3>
-      <p><b>Origin:</b> ${breed.origin || "Unknown"}</p>
-      <p><b>Breed Group:</b> ${breed.breed_group || "Unknown"}</p>
-      <p><b>Bred For:</b> ${breed.bred_for || "Unknown"}</p>
-      <p><b>Temperament:</b> ${breed.temperament || "Unknown"}</p>
-    </div>
-    </div>
-    </div>
-  `;
-} 
 
 fetchData();
+
+console.log(breeds);
+
+//FILTER BREEDS BY BREED GROUP
+
+
+
+// UPDATE FOLLOWING CODE TO WORK AS WELL:
+
+// function dogHTML(breed) {
+//   return `
+//   <div class="user">
+//     <div class="user-card">
+//     <div class="user-card__container">
+//       <h3>${breed.name}</h3>
+//       <p><b>Origin:</b> ${breed.origin || "Unknown"}</p>
+//       <p><b>Breed Group:</b> ${breed.breed_group || "Unknown"}</p>
+//       <p><b>Bred For:</b> ${breed.bred_for || "Unknown"}</p>
+//       <p><b>Temperament:</b> ${breed.temperament || "Unknown"}</p>
+//     </div>
+//     </div>
+//     </div>
+//   `;
+// } 
+
+
 
