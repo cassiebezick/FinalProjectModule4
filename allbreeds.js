6a6ca185fb9712d9a1ec8eb7f88237ed548f0c3a
Cassie Bezick <cassiebezick@gmail.com>
2025-12-22 14:22:28 -0500

    Add files via upload

--- allbreeds.js
+++ allbreeds.js
@@ -3,21 +3,23 @@ const API_KEY = "live_c1LVRF88KDghAq5ImtSiz23of0wDNtl17oANVIuJIdwH3NFG5AZaAAv9eZ
 
 const dogBreedContainer = document.querySelector(".dogbreed-list");
 
+let breeds = [];
+
 async function fetchData() {
-  const response = await fetch(API_URL, {
+  let response = await fetch(API_URL, {
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${API_KEY}`
     }
   });
+  breeds = await response.json();
 
-  const dogBreeds = await response.json();
- // console.log(dogBreeds); // always inspect the data
-
-  dogBreedContainer.innerHTML = dogBreeds
-    .map(breed => dogHTML(breed))
+  dogBreedContainer.innerHTML = breeds.map(breed => dogHTML(breed))
     .join("");
-}
+
+};
+
+fetchData();
 
 function dogHTML(breed) {
   return `
@@ -26,6 +28,7 @@ function dogHTML(breed) {
     <div class="user-card__container">
       <h3>${breed.name}</h3>
       <p><b>Origin:</b> ${breed.origin || "Unknown"}</p>
+      <p><b>Life Span:</b> ${breed.life_span || "Unknown"}</p>
       <p><b>Breed Group:</b> ${breed.breed_group || "Unknown"}</p>
       <p><b>Bred For:</b> ${breed.bred_for || "Unknown"}</p>
       <p><b>Temperament:</b> ${breed.temperament || "Unknown"}</p>
@@ -33,68 +36,100 @@ function dogHTML(breed) {
     </div>
     </div>
   `;
-  
-}
+} 
 
-fetchData();
+//select the select filter element in HTML
+const groupsFilter = document.getElementById('sortBreeds');
 
-async function renderBreeds(filter){
-    
-}
-function filterBreeds(event){
+//Adding event listener
+sortBreeds.addEventListener('change' , (event) => {
+  const filter = event.target.value;
 
-}
+  const sortedBreeds = breeds.sort((a,b) => {
+    if ( filter === "AtoZ") {
+      console.log("it worked");
+      return a.name.localeCompare(b.name);
+    }
+    else if (filter === "ZtoA") {
+      console.log("ZtoA");
+      return b.name.localeCompare(a.name);
+    }
+    else if (filter === "BreedGroup") {
+      console.log("breed group");
+      return a.breed_group.localeCompare(b.breed_group)
+    }
+    else if (filter === "LifeSpan") {
+      console.log("life span");
+      //filter for weight here
+    }
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const API_URL = "https://api.thedogapi.com/v1/breeds";
-// const API_KEY = "live_c1LVRF88KDghAq5ImtSiz23of0wDNtl17oANVIuJIdwH3NFG5AZaAAv9eZNIJ9WQ";
-
-// const dogBreedList = document.querySelector('.dogbreed-list')
-
-// // Fetch Data
-// async function fetchData() {
-//     const dogBreedsData = await fetch(API_URL, {
-//        method: "GET", 
-//        headers: {
-//          "Content-Type": "application/json",
-//          "Authorization": `Bearer ${API_KEY}` 
-//             }
-//         });
-
-// // Parse JSON data
-//     const dogBreedList = await dogBreedsData.json();
-//     console.log("Fetched Data:", dogBreedList);
-
-//     //Accessing Properties
-//     dogBreedList.forEach(breed => {
-// //        console.log(`Breed ID: ${breed.id}, Name: ${breed.name}`);
-
-// function dogHTML(breeds) {
-//  return `
-//        <div class="user-card__container">
-//        <h3>${breed.name}</h4>
-//          <p><b>Origin:</b> ${breed.origin}</p>
-//          <p><b>Bred For:</b> ${breed.bred_for}</p>
-//          <p><b>Temperament:</b> ${breed.temperament}"</p>
-//       </div>
-//     </div>`;
-// }
-//  dogBreedList.innerHTML = dogBreedList.map((breed) => dogHTML(breed)).join("");
-
-//     }); 
-// }
+  dogBreedContainer.innerHTML = sortedBreeds
+    .map(breed => dogHTML(breed))
+    .join("");
+});
 
 
-// fetchData();
+
+//         //     <option value="AtoZ">ALL A to Z</option>
+//         //     <option value="ZtoA">ALL Z to A</option>
+//         //     <option value="BreedGroup">Breed Group</option>
+//         //     <option value="Size">Size</option>
+//         // </select>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const API_URL = "https://api.thedogapi.com/v1/breeds";
+// // const API_KEY = "live_c1LVRF88KDghAq5ImtSiz23of0wDNtl17oANVIuJIdwH3NFG5AZaAAv9eZNIJ9WQ";
+
+// // const dogBreedList = document.querySelector('.dogbreed-list')
+
+// // // Fetch Data
+// // async function fetchData() {
+// //     const dogBreedsData = await fetch(API_URL, {
+// //        method: "GET", 
+// //        headers: {
+// //          "Content-Type": "application/json",
+// //          "Authorization": `Bearer ${API_KEY}` 
+// //             }
+// //         });
+
+// // // Parse JSON data
+// //     const dogBreedList = await dogBreedsData.json();
+// //     console.log("Fetched Data:", dogBreedList);
+
+// //     //Accessing Properties
+// //     dogBreedList.forEach(breed => {
+// // //        console.log(`Breed ID: ${breed.id}, Name: ${breed.name}`);
+
+// // function dogHTML(breeds) {
+// //  return `
+// //        <div class="user-card__container">
+// //        <h3>${breed.name}</h4>
+// //          <p><b>Origin:</b> ${breed.origin}</p>
+// //          <p><b>Bred For:</b> ${breed.bred_for}</p>
+// //          <p><b>Temperament:</b> ${breed.temperament}"</p>
+// //       </div>
+// //     </div>`;
+// // }
+// //  dogBreedList.innerHTML = dogBreedList.map((breed) => dogHTML(breed)).join("");
+
+// //     }); 
+// // }
+
+
+// // fetchData();
